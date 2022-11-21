ca265cce96400fb3b5d3cfb6ee10ee2264bd2242
purva <purvasalvi7@gmail.com>
2022-11-21 09:26:36 -0800

    file name change

--- app/Models/FarmerItemquantityModel.php
+++ app/Models/FarmerItemquantityModel.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class FarmeritemquantityModel extends Model
+class FarmeritemquantityModel extends Model 
 {
     protected $table = 'famer_item_quantity';
 
