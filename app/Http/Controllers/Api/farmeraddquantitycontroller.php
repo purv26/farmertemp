3ac90a60ae79c8e724abdb760dd25116009ac343
purva <purvasalvi7@gmail.com>
2022-12-02 22:59:06 -0800

    new changes

--- app/Http/Controllers/Api/farmeraddquantitycontroller.php
+++ app/Http/Controllers/Api/farmeraddquantitycontroller.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
-use App\Models\FarmerItemquantityModel;
+use App\Models\FarmeritemquantityModel;
 use Illuminate\Http\Request;
 use Validator;
 
@@ -10,13 +10,13 @@ class farmeraddquantitycontroller extends Controller
     
     public function __construct()
     {
-        $this->FarmerItemquantityModel = new FarmerItemquantityModel;
+        $this->FarmeritemquantityModel = new FarmeritemquantityModel;
     }
    
     public function itemquantity(Request $request)
     {
        
-     $user=FarmerItemquantityModel::create([
+     $user=FarmeritemquantityModel::create([
         'categoryname'=>$request->categoryname,
         'quantity' => $request->quantity,
         'categoryimage'=>$request->categoryimage,
@@ -32,19 +32,19 @@ class farmeraddquantitycontroller extends Controller
     }
     public function show()
     {
-        $user=FarmerItemquantityModel::all();
+        $user=FarmeritemquantityModel::all();
         return response()->json($user);
     }
     public function showbyid($id)
     {
         
-        $user=FarmerItemquantityModel::find($id);
+        $user=FarmeritemquantityModel::find($id);
         return response()->json($user);
 
     }
     public function updatebyid(Request $request,$id)
     {
-        $user=FarmerItemquantityModel::find($id);
+        $user=FarmeritemquantityModel::find($id);
         $user->categoryname = $request->input('categoryname');
         $user->quantity = $request->input('quantity');
         $user->categoryimage = $request->input('categoryimage');
@@ -55,7 +55,7 @@ class farmeraddquantitycontroller extends Controller
     }
     public function deletebyid(Request $request,$id)
     {
-        $user=FarmerItemquantityModel::find($id);
+        $user=FarmeritemquantityModel::find($id);
         $user->delete();
         return response()->json($user);
 
