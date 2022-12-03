74bf604e767b5b5113af8348d752d3b96a6884f2
purva <purvasalvi7@gmail.com>
2022-12-03 03:33:20 -0800

    file name change

--- app/Models/user.php
+++ app/Models/user.php
@@ -1,11 +1,11 @@
 <?php
 namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Foundation\Auth\user as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
 
-class User extends Authenticatable
+class user extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
