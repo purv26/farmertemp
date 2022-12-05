dca9964bf160314aef296fac5749fc7e69f4d6fe
purvasalvi <purvasalvi7@gmail.com>
2022-12-05 08:47:48 -0800

    gettrader

--- app/Models/user.php
+++ app/Models/user.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
 
-class User extends Authenticatable
+class user extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
